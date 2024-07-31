39759aa5096889ca7aee63ea76362a22ee1b2ea8
GrafScar94 <64083167+GrafScar94@users.noreply.github.com>
2024-07-31 23:31:59 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Введите строку: ")
+print("Количество символов в строке:", len(my_string))
+print("Строка в верхнем регистре:", my_string.upper())
+print("Строка в нижнем регистре:", my_string.lower())
+print("Корректировка строки:", my_string.replace(' ', ''))
+print("Первый символ строки:", my_string[0])
+print("Последний символ строки:", my_string[-1])
