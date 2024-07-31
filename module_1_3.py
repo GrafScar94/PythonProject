b110bc78f6f9b094f8ca68fc9e7c3c4ded131f41
GrafScar94 <64083167+GrafScar94@users.noreply.github.com>
2024-07-31 22:13:34 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,17 @@
+name = 'Руслан'
+print(name, type(name))
+age = 29
+print(age, type(age))
+age = age + 1
+print(age, type(age))
+is_student = True
+print(is_student, type(is_student))
+print()
+name = 'Руслан'
+print("Имя: " + name)
+age = 29
+print("Возраст: " + str(age))
+age = age + 1
+print("Новый возраст: " + str(age))
+is_student = True
+print("Студент: " + str(is_student))
