7ba7ec8da8498f53d2d5b9a7e55f53d94827a043
Ruslan Galimov <64083167+GrafScar94@users.noreply.github.com>
2025-02-02 13:23:11 +0500

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,9 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    for function in functions:
+        res = function(int_list)
+        results.update({function.__name__: res})
+    return results
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
